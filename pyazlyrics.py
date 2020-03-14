c8d3d2e274f6ce1fed3cd7c080afff48cdec72de
hian <hiancdtrsnm@gmail.com>
2020-03-13 22:35:56 -0400

    Fix path bug and use session

--- pyazlyrics.py
+++ pyazlyrics.py
@@ -13,6 +13,11 @@ FOLDER = 'songs'
 
 os.makedirs(FOLDER,exist_ok=True)
 
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+session = requests.Session()
+
+session.headers.update({'User-Agent': user_agent})
+
 BASE_URL = 'https://www.azlyrics.com/'
 
 def get_song(html: Selector):
@@ -46,52 +51,50 @@ def get_artists(html: Selector):
 manager = enlighten.get_manager()
 
 
-# letter_pages = ['https://www.azlyrics.com/a.html', 'https://www.azlyrics.com/b.html']
-# pbar = manager.counter(total=len(letter_pages), desc='pages', unit='pages')
+letter_pages = ['https://www.azlyrics.com/a.html', 'https://www.azlyrics.com/b.html']
+pbar = manager.counter(total=len(letter_pages), desc='pages', unit='pages')
 
-# for page in letter_pages:
-#     pbar.update()
-#     response = requests.get(page)
-#     s_page = Selector(response.text)
-#     artists = get_artists(s_page)
-#     abar = manager.counter(total=len(artists), desc=f'Page({page})', unit='artists')
+for page in letter_pages:
+    pbar.update()
+    response = session.get(page)
+    s_page = Selector(response.text)
+    artists = get_artists(s_page)
+    abar = manager.counter(total=len(artists), desc=f'Page({page})', unit='artists')
 
-#     print(response.text)
+    for artist in artists:
+        abar.update()
+        response = session.get(artist)
+        s_artist = Selector(response.text)
+        songs = get_songs(s_artist)
+        sbar = manager.counter(total=len(songs), desc=f'Songs of ({artist})', unit='songs')
 
-#     for artist in artists:
-#         abar.update()
-#         response = requests.get(artist)
-#         s_artist = Selector(response.text)
-#         songs = get_songs(s_artist)
-#         sbar = manager.counter(total=len(songs), desc=f'Songs of ({artist})', unit='songs')
+        for song in songs:
+            sbar.update()
+            response = session.get(song)
+            time.sleep(3)
+            s_song = Selector(response.text)
+            song = get_song(s_song)
+            json.dump(song, open(os.path.join(FOLDER, f"({song['title']})-({song['band']}).json"), 'w'), indent=2)
+            print(f"({song['title']})-({song['band']})")
 
-#         for song in songs:
-#             sbar.update()
-#             response = requests.get(song)
-#             time.sleep(3)
-#             s_song = Selector(response.text)
-#             song = get_song(s_song)
-#             json.dump(song, open(os.path.join(FOLDER, f"({song['title']})-({song['band']}).json", 'w')), indent=2)
-#             print(f"({song['title']})-({song['band']})")
-
-#         sbar.close()
-#     abar.close()
+        sbar.close()
+    abar.close()
 
 
-response = requests.get('https://www.azlyrics.com/a.html')
-s = Selector(response.text)
-data = get_artists(s)
+# response = requests.get('https://www.azlyrics.com/a.html')
+# s = Selector(response.text)
+# data = get_artists(s)
 
-print(data)
+# print(data)
 
-response = requests.get('https://www.azlyrics.com/a/amywinehouse.html')
-s = Selector(response.text)
-data = get_songs(s)
+# response = requests.get('https://www.azlyrics.com/a/amywinehouse.html')
+# s = Selector(response.text)
+# data = get_songs(s)
 
-print(data)
+# print(data)
 
-response = requests.get('https://www.azlyrics.com/lyrics/sexpistols/godsavethequeen.html')
-s = Selector(response.text)
-data = get_song(s)
+# response = requests.get('https://www.azlyrics.com/lyrics/sexpistols/godsavethequeen.html')
+# s = Selector(response.text)
+# data = get_song(s)
 
-print(data)
+# print(data)
